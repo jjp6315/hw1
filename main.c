3dcf12cbabbde258d48a5654851b34babed13f1c
jjp6315 <jjp6315@psu.edu>
2020-09-04 00:14:24 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,16 @@
+// Author: Ji Woong Park jjp6315@psu.edu
+// Collaborator: Eric Zhang ecz5032@psu.edu
+// Collaborator: Harmon Piatkowski hjp5276@psu.edu
+// Collaborator: Chris Belt cob5463@psu.edu
+
+#include <stdio.h>
+#include <readline/readline.h>
+#include <stdlib.h>
+
+int main(void) {
+  char * tempstr = readline("Enter temperature in celsius: ");
+  double temp = atof(tempstr);
+  double tempf = temp * 9/5 + 32;
+  printf("%f° in Celsius is equivalant to %f° in Fahrenheit.\n", temp, tempf);
+  return 0;
+}
